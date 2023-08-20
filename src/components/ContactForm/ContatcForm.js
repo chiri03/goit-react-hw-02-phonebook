2e00d10227aade560d658337f6a65f1aa01ac0d8
chiri03 <lena.kiriac.97@gmail.com>
2023-08-20 20:34:53 +0300

    fixxing

--- src/components/ContactForm/ContatcForm.js
+++ src/components/ContactForm/ContatcForm.js
@@ -49,7 +49,8 @@ export class ContactForm  extends Component {
             type="text"
             value={name}
             onChange={this.handleChange}
-            // pattern="^[a-zA-Zа-яА-Я]+((['-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            pattern="^[a-zA-Zа-яА-Я]+((['-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                             
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                       required
                       style={{height: '20px', marginLeft: '10px'}}
@@ -61,7 +62,7 @@ export class ContactForm  extends Component {
             name="number"
             type="tel"
             value={number}
-            // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+           pattern="^\+?\d{1,4}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             onChange={this.handleChange}
                       required
