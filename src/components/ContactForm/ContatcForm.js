43a20d1d73ae4769289090d4409e3d448f2cb47d
chiri03 <lena.kiriac.97@gmail.com>
2023-08-20 20:30:31 +0300

    check

--- src/components/ContactForm/ContatcForm.js
+++ src/components/ContactForm/ContatcForm.js
@@ -44,12 +44,12 @@ export class ContactForm  extends Component {
       <form className="Form" onSubmit={this.handleSubmitForm} key={this.id}>
         <div className="AddContactLabel">
           Name
-          <input className="AddContactInput"
+          <input 
             name="name"
             type="text"
             value={name}
             onChange={this.handleChange}
-            pattern="^[a-zA-Zа-яА-Я]+((['-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            // pattern="^[a-zA-Zа-яА-Я]+((['-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                       required
                       style={{height: '20px', marginLeft: '10px'}}
@@ -57,11 +57,11 @@ export class ContactForm  extends Component {
         </div>
         <div className="AddContactLabel">
           Number
-          <input className="AddContactInput"
+          <input 
             name="number"
             type="tel"
             value={number}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             onChange={this.handleChange}
                       required
